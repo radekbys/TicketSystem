6fc577a9d617e8f88f5d911f5023c686bcdda45e
radekbys <radekbry12@gmail.com>
2025-07-03 17:54:59 +0200

    just finished building backend app, next I will try to host it

---
+++ TicketSystem-backend/TicketSystem/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+
+class Ticket(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, max_length=8
+    )
+    title = models.CharField()
+    content = models.TextField()
+    status = models.CharField()
+    creationDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ TicketSystem-backend/TicketSystem/views.py
@@ -0,0 +1,46 @@
+from django.http import JsonResponse
+from .models import Ticket
+from .serializers import TicketSerializer
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def ticket_list(request):
+    if request.method == "GET":
+        tickets = Ticket.objects.all()
+        serializer = TicketSerializer(tickets, many=True)
+        return JsonResponse({"tickets": serializer.data})
+
+    elif request.method == "POST":
+        serializer = TicketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def ticket_details(request, id):
+    try:
+        ticket = Ticket.objects.get(pk=id)
+    except Ticket.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        serializer = TicketSerializer(ticket, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        ticket.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
